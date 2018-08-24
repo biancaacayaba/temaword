3b8df3c7c4ea9d65c184de69e6e10335e5a9879f
Acayaba <bianca.acayaba@gmail.com>
2018-08-24 14:13:42 +0000

    iniciando parte 2

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>titulo</title>
-</head>
-<body>
-<h1>oi</h1>
-</body>
-</html>
+<?php get_header();?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="jumbotron">
+                <h2>Seja bem vindo ao meu novo tema</h2>
+        </div>
+    </div>
+</div>
+<?php get_footer();?>
