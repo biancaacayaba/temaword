b6f6129135afff0156cc7f26c385bb48bc4b8d4f
Acayaba <bianca.acayaba@gmail.com>
2018-08-27 23:19:34 +0000

    ajustes

--- header.php
+++ header.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale-1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">          
     <title><?php wp_title();?></title>
-    <link rel="stylesheet" href="<?php echo get_template_directiry_url()?> /assets/css/bootstrap/css/bootstrap.min.css" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/bootstrap/css/bootstrap.min.css" type="text/css"/>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" type="text/css"/>
    <?php wp_head();?>
    <div class="container geral">
        <div class="row">
            <div class="col-md-3 logo">
                <a href="<?php echo site_url();?>">
-                   <img src="<?php echo get_template_directory_url()?>/novotema/screenshot.png" class="logo"></img>
+                   <img src="<?php echo get_template_directory_uri()?>/assets/images/logo.png" class="logo"></img>
                </a>
                
            </div>
